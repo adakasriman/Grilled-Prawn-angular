af14061e1591c5e43e62e8c9cb270ab5cb905a34
sriman4 <sriman.a@credright.com>
2022-02-15 13:26:31 +0530

    patchvalue

--- src/app/components/student/student.component.ts
+++ src/app/components/student/student.component.ts
@@ -65,26 +65,26 @@ export class StudentComponent implements OnInit {
   addStudents(student: any) {
 
     let storeData = JSON.parse(localStorage.getItem('students') || '[]');
-    
+
     let index = storeData.findIndex((x: { rollnumber: any; }) => x.rollnumber === student.rollnumber);
     // debugger
     let arr = [];
 
-    if(index !== -1){
+    if (index !== -1) {
       storeData[index] = student;
       localStorage.setItem("students", JSON.stringify(storeData))
-    }else{
-      let lstorageArray =  localStorage.getItem("students");
-      if(lstorageArray){
+    } else {
+      let lstorageArray = localStorage.getItem("students");
+      if (lstorageArray) {
         // debugger;
         this.array = JSON.parse(lstorageArray);
         this.array.push(student);
         localStorage.setItem("students", JSON.stringify(this.array));
-      }else {
+      } else {
         arr.push(student)
         localStorage.setItem("students", JSON.stringify(arr));
       }
-      
+
     }
 
   }
@@ -96,6 +96,13 @@ export class StudentComponent implements OnInit {
       // console.log(this.data);
     }
 
+  }
+
+  editStudentEdit(data: any) {
+    this.userData.patchValue(
+      data
+      // formControlName2: myValue2 (can be omitted)
+    );
   }
 
 
