9c6d5638840c6d72452d163c6a80d117cacb28de
sriman4 <sriman.a@credright.com>
2022-02-17 15:19:33 +0530

    navigate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { Host, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductViewComponent } from './components/product-view/product-view.component';
+import { StudentDataComponent } from './components/student-data/student-data.component';
 import { StudentComponent } from './components/student/student.component';
 
 const routes: Routes = [
@@ -21,7 +22,12 @@ const routes: Routes = [
   {
     path: "student",
     component: StudentComponent
-  }
+  },
+  {
+    path: "/studentData",
+    component: StudentDataComponent
+  },
+
 ];
 
 @NgModule({
